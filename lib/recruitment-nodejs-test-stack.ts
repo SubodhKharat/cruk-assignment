f61c9211080c581bcc2f56c685b99d50ed2725a6
contactsubodhkharat@gmail.com <sbk78266mn>
2024-12-16 08:47:54 +0000

    Created AWS Lambda function for donations

--- lib/recruitment-nodejs-test-stack.ts
+++ lib/recruitment-nodejs-test-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as sns from '@aws-cdk/aws-sns';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class RecruitmentNodejsTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,6 +9,16 @@ export class RecruitmentNodejsTestStack extends cdk.Stack {
     // Create SNS topic
     const donationTopic = new sns.Topic(this, 'DonationTopic', {
       displayName: 'User Donation Thank You Topic'
+    });
+
+    // Create Lambda function
+    const donationFunction = new lambda.Function(this, 'DonationFunction', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        TOPIC_ARN: donationTopic.topicArn
+      }
     });
     
   }
